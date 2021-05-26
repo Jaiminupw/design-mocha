3ed9178d1df54eace4597ac7b1483eadd8311d78
ajaynagotha <ajaynagotha44@gmail.com>
2021-05-26 09:43:29 +0530

    overscroll fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
-import './index.css';
 import 'popper.js/dist/popper.js'
 import reportWebVitals from './reportWebVitals';
 import { Routes } from './routes/Routes';
